21b30a32e6b4eb819e4df000aae681f7c1e7c9e9
pro <quddbscjswow@naver.com>
2022-02-27 23:17:34 +0900

    ControllerAdvice적용

--- src/main/java/hello/exception/api/ApiExceptionV3Controlle.java
+++ src/main/java/hello/exception/api/ApiExceptionV3Controlle.java
@@ -1,18 +1,18 @@
 package hello.exception.api;
 
 import hello.exception.exception.UserException;
-import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api2")
-public class ApiExceptionV2Controller {
+@RequestMapping("/api3")
+public class ApiExceptionV3Controlle {
 
     @GetMapping("/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
